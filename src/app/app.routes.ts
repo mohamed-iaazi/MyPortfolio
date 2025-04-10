c4cb9512546f2f19ab1bab2dcbec6fbedeb0e247
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-10 11:00:27 +0100

    i fix the home responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+    {
+        path:"app-navbar",
+        component :NavbarComponent
+    } ,
+    {
+        path:"app-footer",
+        component :FooterComponent
+    },
+    {
+        path:"app-projets",
+        component :ProjetsComponent
+    } ,
+    {
+        path:"app-home",
+        component :HomeComponent
+    }, 
+    {
+        path:"app-contact",
+        component :ContactComponent
+    } ,
+    {
+        path:"app-about",
+        component :AboutComponent
+
+    }
+];
