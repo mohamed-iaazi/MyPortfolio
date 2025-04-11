e80d45b4a7319b3c47052076b2d846ad1c857a87
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-11 10:26:03 +0100

    i delete unnecceccry codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; // Import the NavbarComponent
 import {FooterComponent} from './footer/footer.component';
-import { HomeComponent} from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjetsComponent } from './projets/projets.component';
  
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,FooterComponent], 
+  imports: [RouterLink, RouterLinkActive,RouterOutlet,NavbarComponent,FooterComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
