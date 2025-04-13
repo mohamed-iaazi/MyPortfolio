7a36e7540f07b6dedb83ec888902701a5448a29f
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-13 13:23:59 +0100

    i add the emailjs libarary to sent an email

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+// contact.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; // Import here
+
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule], // Add ReactiveFormsModule here
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  messageForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.messageForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.messageForm.valid) {
+      const templateParams = {
+        name: this.messageForm.value.name,
+        email: this.messageForm.value.email,
+        message: this.messageForm.value.message
+      };
+
+      emailjs.send('service_42npztl', 'template_jkn4zsa', templateParams, 'bg_MfpuJFsYLgrqeU')
+        .then(() => {
+          alert('Message sent successfully!');
+          this.messageForm.reset();
+        })
+        .catch((error: any) => {
+          console.error('Email sending error:', error);
+          alert('Oops! Something went wrong.');
+        });
+    }
+  }
 }
